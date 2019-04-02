b918780eac13d61a8d06ac884bc61f35bfe78d29
erbina6706174043 <48651444+erbina6706174043@users.noreply.github.com>
2019-04-02 09:06:00 +0700

    Update JurnalMod8.java

--- JurnalMod8.java
+++ JurnalMod8.java
@@ -8,43 +8,33 @@ public class JurnalMod8 {
 
  
     public static void main(String[] args) {
-        
-       // Scanner sc = new Scanner(System.in);
-        //int masukkan = sc.nextInt();
-        
-        //System.out.print("Masukkan Angka  : ");
-        //System.out.println("");
+        System.out.print("masukan angka : "); 
+         Scanner sc = new Scanner(System.in); 
+         
+                int i; 
+                int n = sc.nextInt(); 
  
-        int masukkan ;
-        Scanner data = new Scanner(System.in);
-        System.out.print("Masukkan Angka : ");
-        masukkan = data.nextInt();
+        for (i = 0; i < (2 * n) + 1; i++) { 
  
-        int l = 1;
-        int n = masukkan;
-           for(int i=1;i<=n;i++){
-           for(int k=n;k>i;k--){
-                System.out.print(" ");
-           }
-           
-           for(int j=1;j<=l;j++){
-                System.out.print("*");
-           }
-            l+=2;
-                System.out.println("");
-           }
-            l -= 4;
-            
-        for(int i=(n-1);i>=1;i--){
-            
-            for(int k=i;k<=(n-1);k++){
-            System.out.print(" ");
-        }
-            for(int j=l;j>=1;j--){
-         System.out.print("*");
-        }
-            l-=2;
-        System.out.println("");
-         }
+            for (int j = 0; j < (2 * n) + 1; j++) { 
+ 
+                if (i < n) { 
+                    if ((j > n - i) && (j < n + i)) { 
+                        System.out.print("*"); 
+                    } else { 
+                        System.out.print("0"); 
+                    } 
+                } else { 
+ 
+                    if ((j > i - n) && (j < 3*n-i)) { 
+                        System.out.print("*"); 
+                    } else { 
+                        System.out.print("0"); 
+                    } 
+ 
+                } 
+            } 
+            System.out.println(); 
+       }
     }
 }
